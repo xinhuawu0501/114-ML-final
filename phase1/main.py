29f14bdd252042c473338560fb0b2f4d995eb050
kim <kimnai862@gmail.com>
2025-12-16 19:18:06 +0800

    clean up

--- phase1/main.py
+++ phase1/main.py
@@ -3,9 +3,7 @@ from lightning.pytorch.cli import LightningCLI
 from bert_models import BertClassificationModel
 from datamodule import MIMICClassificationDataModule
 import torch
-import pandas as pd
 import os
-from utils.data_utils import misclassified_ratio
 
 torch.set_float32_matmul_precision('medium') # to increase training speed
 
@@ -18,13 +16,6 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-    # load prediction
-    # mode = ["val", "test", "train"]
-    # model_path = os.path.join("./model_output", mode[0], "predictions.csv")
-    # pred = pd.read_csv(model_path, index_col="hadm_id")
-
-    # misclassified_ratio(prediction=pred)
 
     
 
